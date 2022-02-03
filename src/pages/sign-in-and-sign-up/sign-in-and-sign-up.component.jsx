395df7335fc72c0a1667f1c549fceb6111622a8d
Maiqq <miikafagerstrom@gmail.com>
2022-02-03 06:09:31 +0200

    Sign in page added

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUp = () => (
+    <div className="sign-in-and-sign-uo">
+    <SignIn />
+    </div>
+);
+
+export default SignInAndSignUp;
+
+
+
+
+
